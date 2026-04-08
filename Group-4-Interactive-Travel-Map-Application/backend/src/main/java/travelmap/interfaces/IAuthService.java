16099ffde8052107b029bac54027e3fe15248e17
Wilson Buhendwa <xew12@txstate.edu>
2026-04-08 17:24:18 -0500

    implemented SessionManager

--- Group-4-Interactive-Travel-Map-Application/backend/src/main/java/travelmap/interfaces/IAuthService.java
+++ Group-4-Interactive-Travel-Map-Application/backend/src/main/java/travelmap/interfaces/IAuthService.java
@@ -3,6 +3,8 @@ package travelmap.interfaces;
 import travelmap.model.User;
 
 public interface IAuthService {
+    boolean isLoggedIn(String username, String password);
+    void logout();
     boolean isLoggedIn();
     User getCurrentUser();
 }
