552e77ea3873000cb660f16e78bab67d08bab93b
Wilson Buhendwa <xew12@txstate.edu>
2026-04-12 16:11:17 -0500

    tested auth system successfully

--- Group-4-Interactive-Travel-Map-Application/backend/src/main/java/travelmap/interfaces/IAuthService.java
+++ Group-4-Interactive-Travel-Map-Application/backend/src/main/java/travelmap/interfaces/IAuthService.java
@@ -3,7 +3,7 @@ package travelmap.interfaces;
 import travelmap.model.User;
 
 public interface IAuthService {
-    boolean isLoggedIn(String username, String password);
+    boolean login(String username, String password);
     void logout();
     boolean isLoggedIn();
     User getCurrentUser();
