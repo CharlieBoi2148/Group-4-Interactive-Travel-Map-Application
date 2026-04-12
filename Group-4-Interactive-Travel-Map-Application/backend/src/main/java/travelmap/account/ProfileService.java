c53fe115b8f2062903c51b574d284b722ea1186b
Wilson Buhendwa <xew12@txstate.edu>
2026-04-12 15:41:57 -0500

    implemented ProfileService

--- Group-4-Interactive-Travel-Map-Application/backend/src/main/java/travelmap/account/ProfileService.java
+++ Group-4-Interactive-Travel-Map-Application/backend/src/main/java/travelmap/account/ProfileService.java
@@ -2,9 +2,17 @@ package travelmap.account;
 
 import travelmap.interfaces.*;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 public class ProfileService implements IProfileService {
     private UserRepository userRepository;
-    public void updateProfile(String userId, Object data) {}
+    
+    @Autowired
+    public ProfileService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+}
+    public void updateProfile(String userId, Object data) {
+    userRepository.update(userId, data);
+}
 }
